9ef82c22a21e457454e36c38b7ae7250c1c294dc
Maxim Bilan <maximb.mail@gmail.com>
2018-11-06 12:07:29 +0200

    Added Xcode project for the framework

---
+++ SDK/Blondie/Blondie.h
@@ -0,0 +1,19 @@
+//
+//  Blondie.h
+//  Blondie
+//
+//  Created by Maxim on 11/6/18.
+//  Copyright © 2018 Maksym Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Blondie.
+FOUNDATION_EXPORT double BlondieVersionNumber;
+
+//! Project version string for Blondie.
+FOUNDATION_EXPORT const unsigned char BlondieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Blondie/PublicHeader.h>
+
+
