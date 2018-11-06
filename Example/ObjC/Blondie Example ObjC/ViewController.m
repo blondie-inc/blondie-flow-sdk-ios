d3da0c048a039ae8048bd9b9ec4c94e92c952345
Maxim Bilan <maximb.mail@gmail.com>
2018-11-06 12:59:31 +0200

    Added Obj C example project

---
+++ Example/ObjC/Blondie Example ObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Blondie Example ObjC
+//
+//  Created by Maxim on 11/6/18.
+//  Copyright © 2018 blondie.lv. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
